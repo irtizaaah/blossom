17204fbb43ea907d95fa3aa70692f835543d0ac3
irtizaaah <irtizaaah@gmail.com>
2024-01-13 13:04:57 -0800

    updated example code

--- app.js
+++ app.js
@@ -1,14 +1,26 @@
 import {h1, div, p} from './tagFunctions.js'
 import {Blossom, State} from './blossom.js'
 
-const blossom = new Blossom(); // Creating a single instance of Blossom
+const blossom = new Blossom();
 let counter = new State(0);
 
 const styles = {
+    horizontalBlock: {
+        'display':'flex'
+    },
     button: {
-        'color': 'blue',
-        'backgroundColor' : 'red',
+        'text-align':'center',
+        'color': 'white',
+        'backgroundColor' : 'black',
+        'padding' : '0.5em',
+        'margin' : '0.5em',
         'width' : '5rem'
+    },
+    negativeCounter: {
+        'color':'red'
+    },
+    positiveCounter: {
+        'color':'green'
     }
 }
 
@@ -20,8 +32,12 @@ const customButton = (text, handleClick) => {
 
 const app = () => {
   return div(
-    h1(counter.getState()),
-    customButton("increment", ()=>counter.setState(counter.getState() + 1))
+    h1(counter.getState())
+        .setStyles(counter.getState() < 0 ? styles.negativeCounter:styles.positiveCounter),
+    div(
+        customButton("decrement", ()=>counter.setState(counter.getState() - 1)),
+        customButton("increment", ()=>counter.setState(counter.getState() + 1))
+    ).setStyles(styles.horizontalBlock)
   );
 };
 
